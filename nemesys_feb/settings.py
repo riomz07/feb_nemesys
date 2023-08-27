95adcca23c0ed9e5b7eaad89fee1038835e02663
riomz07 <riomadaraz7@gmail.com>
2023-08-27 23:06:30 +0700

    fix csrf

--- nemesys_feb/settings.py
+++ nemesys_feb/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-cdww6%kqxq1^pdgj^#c-r&!e4n)j6^&t#o&@py*06_l89o^-52
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['10.10.10.61','nemesys.feb.unej.ac.id',"localhost"]
 
 CSRF_TRUSTED_ORIGINS = ['http://10.10.10.61','https://nemesys.feb.unej.ac.id']
 
