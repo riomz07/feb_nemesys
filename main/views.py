4d95b86c1c0e5c38e61d3d33ce1a47ec04511eba
riomz07 <riomadaraz7@gmail.com>
2023-08-31 10:11:12 +0700

    fix monitor

--- main/views.py
+++ main/views.py
@@ -212,7 +212,7 @@ def netmaps_data(request):
 def services(request):
     # Eksekusi perintah
     try:
-        command = "systemctl status nginx | grep 'Active:' | awk '{print $2}'"
+        command = "systemctl status monitor | grep 'Active:' | awk '{print $2}'"
         result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, text=True)
         result_mes = result.stdout
     except Exception as e:
